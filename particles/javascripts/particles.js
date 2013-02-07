018ee1d5092ace7d90808a72c0e94d2e71a7332b
Jack Lawson <ajacksified@gmail.com>
2013-02-07 13:21:59 -0800

    add z scale to particles

--- particles/javascripts/particles.js
+++ particles/javascripts/particles.js
@@ -1,13 +1,16 @@
-var z = d3.scale.category20c(),
+var scale = d3.scale.category20c(),
     i = 0;
 
 // original position
 var originX = 600;
 var originY = 300;
+var originZ = 200;
 var posX, posY;
 
-var maxX = 600;
-var maxY = 300;
+var maxX = originX;
+var maxY = originY;
+var maxZ = originZ;
+
 var maxSpan = 100;
 
 function translateXScreenDistance(distance) {
@@ -18,29 +21,35 @@ function translateYScreenDistance(distance) {
   return maxY - ((distance / maxSpan) * maxY);
 }
 
+function translateZScreenDistance(distance){
+  return (distance / maxSpan) * maxZ;
+}
+
 var svg = d3.select("body").append("svg:svg");
 
-function particle(x, y) {
+function particle(x, y, z) {
   svg.append("svg:circle")
       .attr("cx", x)
       .attr("cy", y)
-      .attr("r", 1e-6)
-      .style("stroke", z(++i))
-      .style("stroke-opacity", 1)
+      .attr("r", 1e-6 + (z / 5))
+      .style("stroke", scale(++i))
+      .style("stroke-opacity", 1 - ((maxZ -z) / 1000))
     .transition()
       .duration(2000)
       .ease(Math.sqrt)
-      .attr("r", 100)
+      .attr("r", 100 + (z / 5))
       .style("stroke-opacity", 1e-6)
       .remove();
 }
 
 var pointer;
+
 Leap.loop(function(frame) {
   for (var i = 0; i < frame.pointables.length; i++) {
     pointer = frame.pointables[i];
     posX = originX + translateXScreenDistance(pointer.tipPosition[0]);
     posY = originY + translateYScreenDistance(pointer.tipPosition[1]);
-    particle(posX, posY);
+    posZ = originZ + translateZScreenDistance(pointer.tipPosition[2]);
+    particle(posX, posY, posZ);
   }
 });
